c69547510af13218366b67fc6f65ddc7646234af
Georgi Haydukov <georgi.haydukov99@gmail.com>
2023-06-01 13:42:47 +0200

    Added a model for the income

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,1 +1,7 @@
-const router = require("express").Router;
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello AGAIN!");
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { db } = require("./db/db");
+const { readdirSync } = require("fs");
 
 const app = express();
 
@@ -11,6 +12,11 @@ const PORT = process.env.PORT;
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// Routes
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
