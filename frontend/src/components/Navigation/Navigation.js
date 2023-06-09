3c4d927416d3bc9faa0f2517987154ed9aa439da
Georgi Haydukov <georgi.haydukov99@gmail.com>
2023-06-09 13:26:03 +0200

    Added all of the icons

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { styled } from "styled-components";
+import avatar from "../../img/avatar.jpg";
 
 function Navigation() {
-  return <NavigationStyled></NavigationStyled>;
+  return (
+    <NavigationStyled>
+      <div className="user-container">
+        <img src={avatar} />
+
+        <div className="text">
+          <h2>Georgi</h2>
+
+          <p>Your Money</p>
+        </div>
+      </div>
+
+      <div className="menu-items"></div>
+    </NavigationStyled>
+  );
 }
 
 const NavigationStyled = styled.nav``;
