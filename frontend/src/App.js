f671187fb5070e69b513298a3857b6928ee87d45
Georgi Haydukov <georgi.haydukov99@gmail.com>
2023-06-09 12:39:38 +0200

    Frontend setup part 1 and minor backend changes on the requests.rest file.

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+import bg from "./img/background.jpg";
+import { MainLayout } from "./styles/Layouts";
+
+function App() {
+  return (
+    <AppStyled bg={bg} className="App">
+      <MainLayout>
+        <h1>Hello</h1>
+      </MainLayout>
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${(props) => props.bg});
+  position: relative;
+`;
+
+export default App;
